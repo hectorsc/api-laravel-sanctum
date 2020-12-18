32a625a932642c2612c4b876a0335bc9c443806f
Héctor Santiago <makarron90@gmail.com>
2020-12-18 18:07:37 +0100

    CRUD Product

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    
+    public function index()
+    {
+        return Product::all();
+    }
+
+    public function store(Request $request)
+    {
+        $product = Product::create($request->all());
+        return $product;
+         
+    }
+
+    public function show(Product $product)
+    {
+        return $product;  
+    }
+
+    public function update(Request $request, Product $product)
+    {
+        $product->update($request->all());
+    }
+
+    public function destroy(Product $product)
+    {
+        $product->delete();
+        return response()->json();
+    }
+}
