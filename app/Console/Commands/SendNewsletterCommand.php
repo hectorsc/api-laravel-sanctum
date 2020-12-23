3459ba64a6dc20376a5dfaa4a7b6a93ac63d0c7d
Héctor Santiago <makarron90@gmail.com>
2020-12-23 11:58:39 +0100

    tareas programadas, Console/Kernel.php

--- app/Console/Commands/SendNewsletterCommand.php
+++ app/Console/Commands/SendNewsletterCommand.php
@@ -5,40 +5,64 @@ namespace App\Console\Commands;
 use App\Notifications\NewsletterNotification;
 use Illuminate\Console\Command;
 use App\User;
+use App\Product;
+use Illuminate\Support\Facades\DB;
 
 class SendNewsletterCommand extends Command
 {
     // agregamos un argumento al comando opcional para enviar
     // solo a los correos deseados de tipo array => * opcional => ?
-    protected $signature = 'send:newsletter {emails?*}';
-    protected $description = 'Envía un correo electrónico con la newsletter';
+    protected $signature = 'send:newsletter 
+                            {emails?*} : Correos Electrónicos a los cuales enviar directamente
+                            {--s|schedule : Si debe ser ejecutado directamente o no}';
+    protected $description = 'Envía un correo electronico a todos los usuarios que hayan verificado su cuenta';
+
+    
+    public function __construct()
+    {
+        parent::__construct();
+    }
 
     public function handle()
     {
-        $emails = $this->argument('emails');
+        $userEmails = $this->argument('emails');
+        $schedule = $this->option('schedule');
+
         $builder = User::query();
 
-        if ($emails) {
-            $builder->whereIn('email', $emails);
+        if ($userEmails) {
+            $builder->whereIn('email', $userEmails);
         }
 
+        $builder->whereNotNull('email_verified_at');
         $count = $builder->count();
-        
-        // si tenemos usuarios enviamos la notificiación
-        if ($count) {
-            $this->output->progressStart($count);
-            // usuarios que hayan verificado su correo electronico
-            $builder->whereNotNull('email_verified_at')
-                ->each(function (User $user) {
-                    $user->notify(new NewsletterNotification());
-                    $this->output->progressAdvance();
-                    $this->info("\n". ' Usuario verificado, enviamos correo');
-                });
-            $this->output->progressFinish();
-            $this->info("De los {$count} usuarios registrados en el sistema, se enviaron {$builder->count()} correos");
 
-        } else {
-            $this->info("\n" . 'No se envió ningun correo');
+        if ($count) {
+            $this->info("Se enviarán {$count} correos");
+
+            if ($this->confirm('¿Estás de acuerdo?') || $schedule) {
+
+                $productQuery = Product::query();
+                // Productos mejor calificados
+                $productQuery->withCount(['qualifications as average_rating' => function ($query) {
+                    $query->select(DB::raw('coalesce(avg(score),0)'));
+                }])->orderByDesc('average_rating');
+
+                $products = $productQuery->take(6)->get();
+
+                $this->output->progressStart($count);
+
+                $builder->each(function (User $user) use ($products) {
+                    $user->notify(new NewsletterNotification($products->toArray()));
+                    $this->output->progressAdvance();
+                });
+
+                $this->output->progressFinish();
+                $this->info('Correos enviados');
+                return;
+            } 
         }
+
+        $this->info('No se enviaron correos'); 
     }
 }
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,8 @@
 
 namespace App\Console;
 
+use App\Console\Commands\SendEmailVerificationReminderCommand;
+use App\Console\Commands\SendNewsletterCommand;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -13,7 +15,8 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        SendNewsletterCommand::class,
+        SendEmailVerificationReminderCommand::class,
     ];
 
     /**
@@ -24,7 +27,31 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+        // // definimos las tareas programadas
         // $schedule->command('inspire')->hourly();
+        $schedule->command('inspire')
+            // comando se ejecuta en modo mantenimiento
+            // y se activa con php artisan down
+            // php artisan up se para .... duda??
+            ->evenInMaintenanceMode()
+            // para hacer un log de lo que está pasando
+            ->sendOutputTo(storage_path('inspire.log'))
+            ->everyMinute();
+        // // Podemos ejecutar comandos usando funciones anonimas
+        // $schedule->call(function (){
+        //     echo "hola";
+        // })->everyFiveMinutes();
+        $schedule->command(SendNewsletterCommand::class)
+            // evita la superposicion de tareas
+            // si ya hay una tarea de la misma instancia corriendo no se ejecuta
+            ->withoutOverlapping()
+            //por si nuestra aplicacion se ejecuta en varios servidores con esto sólo lo hará en uno
+            ->onOneServer() 
+            ->mondays();
+        $schedule->command(SendEmailVerificationReminderCommand::class)
+            ->onOneServer()
+            ->daily();
+        
     }
 
     /**
